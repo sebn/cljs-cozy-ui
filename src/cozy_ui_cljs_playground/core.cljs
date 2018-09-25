943a8b79b72e1ebf36d1f95e72ba1bdf4635f73a
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2018-09-25 22:04:09 +0200

    Layout demo

--- src/cozy_ui_cljs_playground/core.cljs
+++ src/cozy_ui_cljs_playground/core.cljs
@@ -1,16 +1,38 @@
 (ns cozy-ui-cljs-playground.core
   (:require [reagent.core :as reagent :refer [atom]]
-            ["cozy-ui/dist/react/Button" :refer [Button]]))
+            ["cozy-ui/dist/react/Button" :refer [Button]]
+            ["cozy-ui/dist/react/Layout" :refer [Content Layout Main]]
+            ["cozy-ui/dist/react/Nav" :default Nav :refer [NavIcon NavItem NavText genNavLink]]
+            ["cozy-ui/dist/react/Sidebar" :default Sidebar]))
 
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+(def NavLink
+  (genNavLink
+   #(reagent/create-element "a"
+                            #js {:className (.-className %)}
+                            (.-children %))))
+
 (defn hello-world []
-  [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]
-   [:> Button {:label "Click me" :theme "regular"} ""]])
+  [:> Layout
+   [:> Sidebar
+    [:> Nav
+     [:> NavItem
+      [:> NavLink
+       [:> NavIcon {:icon "warn"}]
+       [:> NavText "Warn"]]]
+     [:> NavItem
+      [:> NavLink
+       [:> NavIcon {:icon "check"}]
+       [:> NavText "Check"]]]
+     [:> NavItem
+      [:> NavLink
+       [:> NavIcon {:icon "download"}]
+       [:> NavText "Download"]]]]]
+   [:> Main
+    [:> Content]]])
 
 (defn start []
   (reagent/render-component [hello-world]
